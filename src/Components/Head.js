3c962d47605e4980bf93a81cceeb03044c0d4d1f
David <odavidbolaji14@gmail.com>
2021-11-30 05:17:58 +0100

    fixed toggle

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -18,20 +18,20 @@ const Head = (props) => {
                 <p>theme</p>
             </div>
             <div className={props.togglerCont} style={{position: 'relative'}}>
-                <div className={props.tracker}>
+                <div className={props.tracker} style={{position:'absolute',right:'10px',top: '19px',zIndex:5, opacity:1}}>
                     <span  className={props.spanGrp} onClick={() => clickHandler(1)}>1</span>
                     <span  className={props.spanGrp} onClick={() => clickHandler(2)}>2</span>
                     <span  className={props.spanGrp} onClick={() => clickHandler(3)}>3</span>
                 </div>
-                <div className={props.tracker} style={{position:'absolute',right:'10px',top: '35px',zIndex:5, opacity:0}}>
+                <div className={props.tracker} style={{position:'absolute',right:'10px',top: '34px',zIndex:5, opacity:0}}>
                     <span  className={props.spanGrp} onClick={() => clickHandler(1)}>1</span>
                     <span  className={props.spanGrp} onClick={() => clickHandler(2)}>2</span>
                     <span  className={props.spanGrp} onClick={() => clickHandler(3)}>3</span>
                 </div>
                 
-                <div className={props.togglerGrp} >
+                <div className={props.togglerGrp} style={{position: 'relative'}}>
                     <input type="checkbox" name="toggler" id="toggler" className={props.tog} />
-                    <label htmlFor="toggler" className={props.labelGrp} className={props.label}></label>
+                    <label htmlFor="toggler" className={props.labelGrp} className={props.label} style={{position:'absolute',right:'-5px',top: '55px',zIndex:3, transform: 'translateY(10px)'}}></label>
                 </div>
             </div>
         </div>
